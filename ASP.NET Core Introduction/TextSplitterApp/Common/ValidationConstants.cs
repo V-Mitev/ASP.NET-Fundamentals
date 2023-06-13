d8395a803462f8d0932b94f672888532e0510bc6
Vladimir Mitev <vmitev2001@abv.bg>
2023-06-13 12:14:28 +0300

    Add TextSplitterApp

---
+++ ASP.NET Core Introduction/TextSplitterApp/Common/ValidationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace TextSplitterApp.Common
+{
+    public class ValidationConstants
+    {
+        public const int TextMinimumLength = 2;
+        public const int TextMaximumLength = 30;
+    }
+}
---
+++ ASP.NET Core Introduction/TextSplitterApp/ViewModels/Home/TextViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TextSplitterApp.Common;
+
+namespace TextSplitterApp.ViewModels.Home
+{
+    public class TextViewModel
+    {
+        [Required]
+        [StringLength(ValidationConstants.TextMaximumLength,
+            MinimumLength = ValidationConstants.TextMinimumLength,
+            ErrorMessage = 
+            "The field Text must be a string with a minimum length of 2 and maximum length of 30.")]
+        public string Text { get; set; } = null!;
+
+        public string SplitText { get; set; } = null!;
+    }
+}
