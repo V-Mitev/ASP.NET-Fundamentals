7eb0e75123dffe8e13eab3e71dbbfe6aee74edac
Vladimir Mitev <vmitev2001@abv.bg>
2023-06-24 15:37:02 +0300

    Delete file

--- WorkShop-TaskBoardApp/TaskBoardApp/TaskBoardApp/Contracts/IBoardService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TaskBoardApp.Models.Board;
-
-namespace TaskBoardApp.Contracts
-{
-    public interface IBoardService
-    {
-        public Task<ICollection<BoardViewModel>> AllAsync();
-    }
-}
--- WorkShop-TaskBoardApp/TaskBoardApp/TaskBoardApp/Models/Task/TaskBoardModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaskBoardApp.Models.Task
-{
-    public class TaskBoardModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; } = null!;
-    }
-}
